cf4003336b8268975fed88faedfda7875893dc2e
ournameis <36738905+ournameis@users.noreply.github.com>
2019-08-14 17:34:27 +0530

    Update imageprocessing.py

--- imageprocessing.py
+++ imageprocessing.py
@@ -4,12 +4,9 @@ import os
 
 import cv2
 import tensorflow as tf
-from tensorflow.keras.datasets import cifar10
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
-from tensorflow.keras.callbacks import TensorBoard
 DATADIR = "C:/Users/IP510/Desktop/PetImages"
 
 CATEGORIES = ["Dog", "Cat"]
